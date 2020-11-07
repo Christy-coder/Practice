634dfff1ad759ffb10d6a4e82bca1289f03b352d
Christelle Desire <christelledesire42@yahoo.com>
2020-11-06 23:10:18 -0500

    more functionality

--- client/index.js
+++ client/index.js
@@ -1,33 +1,84 @@
-
-  function postTask() {
-    //get & store the task and description
-    //document query selector for the input boxes
-    const task = document.querySelector("#input1").value;
-    const description = document.querySelector("#input2").value;
-    //fetch request
-    fetch("/task", {
-      //post
-      method: "POST", // or 'PUT'
-      //the headers specify what kind of info we are sending to the server
-      headers: {
-        "Content-Type": "application/json",
-      },
-      //send {task: task, description: description}
-      body: JSON.stringify({ task: task, description: description }),
+function postTask() {
+  //get & store the task and description
+  //document query selector for the input boxes
+  const task = document.querySelector("#input1").value;
+  const description = document.querySelector("#input2").value;
+  //fetch request
+  fetch("/task", {
+    //post
+    method: "POST", // or 'PUT'
+    //the headers specify what kind of info we are sending to the server
+    headers: {
+      "Content-Type": "application/json",
+    },
+    //send {task: task, description: description}
+    body: JSON.stringify({ task: task, description: description }),
+  })
+    //the server has responded! convert the response to json
+    .then((response) => response.json())
+    //if the server responds with data, we will console log it here
+    .then((data) => {
+      console.log("Success:", data);
     })
-      //the server has responded! convert the response to json
-      .then((response) => response.json())
-      //if the server responds with data, we will console log it here
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      //if server sent an error, log it!
-      .catch((error) => {
-        console.error("Error:", error);
+    //if server sent an error, log it!
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+  // '/profile'
+  //send it the task and description
+  //error handling & result handling after the fetch request
+}
+
+//function that does a fetch request to '/task'
+function items() {
+  fetch("/task")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.forEach((el, index) => {
+        singleItems(el);
       });
+    });
+}
 
-    // '/profile'
-    //send it the task and description
-    //error handling & result handling after the fetch request
-  }
+//function to create the div + info
+function singleItems(object) {
+  //create div
+  let div = document.createElement("div");
+  //create a p element
+  let para = document.createElement("p");
+  //assign the inner html
+  para.innerHTML = `Task:${object.task} , Description: ${object.description}`;
+  //create a button
+  let button = document.createElement("button");
+  //assign inner html
+  button.innerHTML = `delete item`;
+  //set onclick attribute
+  button.onclick = deleteItem;
+  //set the id of the button to be the id of the object
+  button.setAttribute('id', object.id);
+  //append the p, button to div
+  div.append(para, button);
+  //append the div to the container (a div prev. set up on the index.html page)
+  let container = document.querySelector(".container");
+  container.append(div);
+}
 
+//create a delete function
+function deleteItem(event) {
+  //fetch request, pass id in body
+  //endpoint is task, method is delete, specifying that we're sending json data, send the id in the body as an object: ex// {id: 4}
+  fetch('/task', {
+  method: 'DELETE',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({id: event.target.id}) 
+})
+.then(res => res.text()) // or res.json()
+.then(res => console.log(res))
+  console.log(event.target.id);
+}
--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
-//require models 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express()
-const path = require('path')
-const toDoController = require('./controllers/toDoController.js')
+//require models
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+const toDoController = require("./controllers/toDoController.js");
 //set up global middleware to parse all request bodies as json
 //run before any other routes, go to the next middleware
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '../client/')))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/")));
 //get request to home page '/' ENTRY, server the index.html file
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   //we're going to send back the index.html file located in the client folder
-  res.sendFile(path.join(__dirname, '../client/index.html'));
-})
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
 //get all of the tasks in the table and send back to front end
 /**STEPS
@@ -21,28 +21,35 @@ app.get('/', (req, res) => {
  * send it back to the server
  * from server, send the data to the client (go to index.js!!)
  */
-app.get('/task', toDoController.addRead, (req, res) => {
-  res.json(res.locals.read)
-})
+app.get("/task", toDoController.addRead, (req, res) => {
+  res.json(res.locals.read);
+});
 
 //post request adding new task '/profile' (CREATE)
-app.post('/task', toDoController.addNew, (req, res) => {
-  res.json('hello')
-})
+app.post("/task", toDoController.addNew, (req, res) => {
+  res.json("hello");
+});
 //update request '/task' (UPDATE)
 
-app.patch('/task', toDoController.update, (req, res) => {
-  res.json('it worked')
-})
+app.patch("/task", toDoController.update, (req, res) => {
+  res.json("it worked");
+});
 
 //delete requesst '/task' (DELETE)
-app.delete('/task', toDoController.delete, (req, res) => {
-  res.json('deleted')
-})
+app.delete("/task", toDoController.delete, (req, res) => {
+  res.json("deleted");
+});
 
 //route is bad -- 404 request, page not found
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
 
 //global middleware errors
+app.use((err, req, res, next) => {
+  console.log("middleware error");
+  return res.sendStatus(500)
+});
 
 //listens for reuqests at port 3000
-app.listen(3000)
+app.listen(3000);
